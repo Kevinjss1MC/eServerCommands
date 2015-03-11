cb06f041ddc9a992a73b6eb764f46822f37a4eb7
Kevinjss1MC <Kevin@Joels-MacBook-Pro-Upstairs-2.local>
2015-03-11 17:21:49 -0400

    Fixed some issues with the plugin.yml and heal command.

--- src/org/kevinjss1/bukkit/eServerCommands/main/Heal.java
+++ src/org/kevinjss1/bukkit/eServerCommands/main/Heal.java
@@ -2,10 +2,11 @@ package org.kevinjss1.bukkit.eServerCommands.main;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Heal extends Main {
+public class Heal extends Main implements CommandExecutor{
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 	Player player = (Player) sender;
